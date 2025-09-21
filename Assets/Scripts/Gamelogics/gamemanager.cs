0e62318fbf8077272dbe2ac8a69e55517fcecfff
THETACCC <qingyuanfan03@gmail.com>
2025-09-21 15:41:44 -0400

    Updated Game Manager

--- Assets/Scripts/Gamelogics/gamemanager.cs
+++ Assets/Scripts/Gamelogics/gamemanager.cs
@@ -3,19 +3,27 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class gamemanager : MonoBehaviour
+public class Gamemanager : MonoBehaviour
 {
+    public static Gamemanager instance;
     //objects don;t destroy
     public GameObject virtualplayer;
     public GameObject playermanager;
     public GameObject avatarplayer;
     public GameObject m_camera;
 
-
-
+    public GamePhase phase;
     private void Awake()
     {
-        DontDestroyOnLoad(this);
+        if (instance == null)
+        {
+            instance = this;
+            DontDestroyOnLoad(gameObject);
+        }
+        else
+        {
+            Destroy(gameObject);
+        }
         DontDestroyOnLoad(virtualplayer);
         DontDestroyOnLoad(avatarplayer);
         DontDestroyOnLoad(m_camera);
@@ -23,4 +31,12 @@ public class gamemanager : MonoBehaviour
     }
 
 
+}
+
+public enum GamePhase
+{
+    Loading,
+    Moving,
+    Talking,
+    Eventing
 }
