df49c4bb13cf4827a0c575a3df703fb97c3a4fb7
HenkFan <129189902+THETACCC@users.noreply.github.com>
2025-09-22 11:55:22 -0400

    Updated Scenes

---
+++ Assets/Scripts/DialogueCollisionTrigger.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DialogueCollisionTrigger : MonoBehaviour
+{
+    public TextAsset jsonFile;
+    private bool isTrigger = false;
+    // Start is called before the first frame update
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Gamemanager.instance.StartDialogue();
+            DialogueManager.instance.LoadDialogueFromResources(jsonFile);
+            DialogueManager.instance.StartDialogue();
+        }
+    }
+}
