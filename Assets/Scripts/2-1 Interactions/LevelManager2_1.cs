37789abb2130af3508c0a66426a53c7d6ab506b8
HenkFan <129189902+THETACCC@users.noreply.github.com>
2025-09-24 12:47:07 -0400

    created Loops

---
+++ Assets/Scripts/2-1 Interactions/LevelManager2_1.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager2_1 : MonoBehaviour
+{
+    public bool isZhouShu = false;
+    
+
+
+    public bool isPlayerEscaped = false;
+
+    //Scene Related
+
+    public string SceneName_NotEscaped;
+    public int SpawnPointLocation_NotEscaped;
+
+    public string SceneName_Escaped;
+    public int SpawnPointLocation_Escaped;
+
+    public ScenePortal BathroomPortal;
+
+    public void SpeakZhoushu()
+    {
+        isZhouShu = true;
+        BathroomPortal.scenename = SceneName_Escaped;
+        BathroomPortal.SpawnPointLocation = SpawnPointLocation_Escaped;
+    }
+
+    public void Start()
+    {
+        BathroomPortal.scenename = SceneName_NotEscaped;
+        BathroomPortal.SpawnPointLocation = SpawnPointLocation_NotEscaped;
+    }
+
+    public void Update()
+    {
+
+    }
+
+}
