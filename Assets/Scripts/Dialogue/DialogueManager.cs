461be13a5a68d568b2db6ec1eb921c8b46b67700
Yoimiyya <hz3735@nyu.edu>
2025-11-12 13:02:07 -0500

    暴露Color接口

--- Assets/Scripts/Dialogue/DialogueManager.cs
+++ Assets/Scripts/Dialogue/DialogueManager.cs
@@ -154,13 +154,13 @@ public class DialogueManager : MonoBehaviour
                     //NPCAvatar.gameObject.SetActive(speakerName != DialogueSettings.instance.playerName);
                     if (currentConversation.isPlayer)
                     {
-                        NPCAvatar.color = Color.gray;
+                        NPCAvatar.color = DialogueSettings.instance.inactiveAvatarColor;
                         avatar.color = Color.white;
                         avatar.sprite = s;
                     }
                     else
                     {
-                        avatar.color = Color.gray;
+                        avatar.color = DialogueSettings.instance.inactiveAvatarColor;
                         NPCAvatar.color = Color.white;
                         NPCAvatar.sprite = s;
                     }
