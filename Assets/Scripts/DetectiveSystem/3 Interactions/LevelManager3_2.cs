ec90d4133af515398e0c5665c1b8ad9a7f423768
Jasminezhang666666 <129326223+Jasminezhang666666@users.noreply.github.com>
2025-11-14 19:17:03 -0500

    2 bugs fixed, sprite updated

---
+++ Assets/Scripts/DetectiveSystem/3 Interactions/LevelManager3_2.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelManager3_2 : MonoBehaviour
+{
+    [SerializeField] private bool verboseLog = false;
+
+    private void Start()
+    {
+        // 用 tag 找到 DontDestroyOnLoad 的 Player
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        if (!player)
+        {
+            Debug.LogWarning("[LevelManager3_2] 没有找到 tag=Player 的对象。");
+            return;
+        }
+
+        // 把她身上（以及子物体上）的所有 SpriteRenderer 打开
+        SpriteRenderer[] sprites = player.GetComponentsInChildren<SpriteRenderer>(true);
+        foreach (var sr in sprites)
+        {
+            if (sr != null)
+                sr.enabled = true;
+        }
+
+        if (verboseLog)
+            Debug.Log($"[LevelManager3_2] 已为 Player 重新启用 {sprites.Length} 个 SpriteRenderer。");
+    }
+}
