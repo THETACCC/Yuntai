168ed50305e20ae6f445cc5ca9f83527dc925dce
HenkFan <129189902+THETACCC@users.noreply.github.com>
2026-01-28 14:39:25 -0500

    ghost update

--- Assets/Scripts/MiniGames/GhostChase.cs
+++ Assets/Scripts/MiniGames/GhostChase.cs
@@ -4,15 +4,52 @@ using UnityEngine;
 
 public class GhostChase : MonoBehaviour
 {
-    // Start is called before the first frame update
+    [Header("Movement")]
+    public float moveSpeed = 2.5f;
+
+    private Transform player;
+    private Rigidbody2D rb;
+
+    //Movement Controll
+    public bool isAllowMove = false;
+
     void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
+
+        GameObject playerObj = GameObject.FindGameObjectWithTag("Player");
+        if (playerObj != null)
+        {
+            player = playerObj.transform;
+        }
+        else
+        {
+            Debug.LogError("Player not found. Make sure the Player has the 'Player' tag.");
+        }
     }
 
-    // Update is called once per frame
-    void Update()
+    void FixedUpdate()
     {
-        
+        if (player == null) return;
+
+        float directionX = player.position.x - transform.position.x;
+
+        if(isAllowMove)
+        {
+            // Move only horizontally
+            rb.velocity = new Vector2(Mathf.Sign(directionX) * moveSpeed, rb.velocity.y);
+
+            // Optional: flip sprite based on direction
+            if (directionX != 0)
+            {
+                transform.localScale = new Vector3(
+                    Mathf.Sign(directionX),
+                    1,
+                    1
+                );
+            }
+        }
+
+
     }
 }
