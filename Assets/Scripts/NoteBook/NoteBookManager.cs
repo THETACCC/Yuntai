8dfa339fd35fd4e40f892f937aa6c77384d491dc
THETACCC <qingyuanfan03@gmail.com>
2026-02-23 16:49:22 -0500

    Updated Audio

--- Assets/Scripts/NoteBook/NoteBookManager.cs
+++ Assets/Scripts/NoteBook/NoteBookManager.cs
@@ -2,6 +2,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static AudioManager;
 
 public class NoteBookManager : MonoBehaviour
 {
@@ -128,6 +129,37 @@ public class NoteBookManager : MonoBehaviour
     {
         isOpen = false;
     }
+
+    //Audio Related
+    public void NoteBookInteraction()
+    {
+        int myRandomNum = Random.Range(0, 5);
+        if (myRandomNum == 0)
+        {
+            AudioManager.Play("Sound Effects/Chapter1/sndNoteBook1", AudioGroup.SFX);
+        }
+        else if (myRandomNum == 1)
+        {
+            AudioManager.Play("Sound Effects/Chapter1/sndNoteBook2", AudioGroup.SFX);
+        }
+        else if (myRandomNum == 2)
+        {
+            AudioManager.Play("Sound Effects/Chapter1/sndNoteBook3", AudioGroup.SFX);
+        }
+        else if (myRandomNum == 3)
+        {
+            AudioManager.Play("Sound Effects/Chapter1/sndNoteBook4", AudioGroup.SFX);
+        }
+        else if (myRandomNum == 4)
+        {
+            AudioManager.Play("Sound Effects/Chapter1/sndNoteBook5", AudioGroup.SFX);
+        }
+        else if (myRandomNum == 5)
+        {
+            AudioManager.Play("Sound Effects/Chapter1/sndNoteBook6", AudioGroup.SFX);
+        }
+    }
+
 
     /// <summary>
     /// 获取NoteBookData的CSV文本
