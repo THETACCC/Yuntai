04a8ddb2e4cb8ada29c191687834b012f68bc666
HenkFan <129189902+THETACCC@users.noreply.github.com>
2026-04-08 14:52:36 -0400

    Updated Monster Sound

--- Assets/Scripts/MiniGames/GhostChase.cs
+++ Assets/Scripts/MiniGames/GhostChase.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static AudioManager;
 
 public class GhostChase : MonoBehaviour
 {
@@ -72,6 +73,7 @@ public class GhostChase : MonoBehaviour
     public void EnableGhost()
     {
         isAllowMove = true;
+        AudioManager.Play("Sound Effects/sndMonsterRoar", AudioGroup.SFX);
     }
 
     public void OnTriggerEnter2D(Collider2D collision)
